3e7fe8bbf9e01c73b35a7b9d37707948f024bc79
Ewelina Z <narcyza1989@o2.pl>
2020-04-30 18:13:35 +0200

    Adding bootstrap for data. Closes #14

--- pet-clinic-web/src/main/java/com/example/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/example/petclinic/controllers/VetController.java
@@ -3,11 +3,11 @@ package com.example.petclinic.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
+@RequestMapping("/vets")
 @Controller
 public class VetController {
 
-    @RequestMapping({"vets", "vets/index", "vets/index.html"})
+    @RequestMapping({"","/", "vets/index", "vets/index.html"})
     public String listVets(){
         return "vets/index";
     }
