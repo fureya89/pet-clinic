12dd33120a568f6710196bc73ae1fd472a9b1e1c
Ewelina Z <narcyza1989@o2.pl>
2020-05-18 20:57:40 +0200

    Fixed Broken Links. Closes #45

--- pet-clinic-web/src/main/java/com/example/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/example/petclinic/controllers/VetController.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/vets")
 @Controller
 public class VetController {
 
@@ -18,11 +17,10 @@ public class VetController {
     }
 
 
-    @RequestMapping({"","/", "index", "index.html"})
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html", "/vets.html"})
     public String listVets(Model model){
 
         model.addAttribute("vets", vetService.findAll());
-
         return "vets/index";
     }
 }
